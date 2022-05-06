00cb2eca3a5d01375753a7cc524f90f3ab8e051a
atriumrules <102473631+atriumrules@users.noreply.github.com>
2022-05-06 19:56:48 +0100

    sddssd

--- loader.lua
+++
@@ -1,7 +0,0 @@
-local key = _G.Key
-local check = "checkhere?key=" .. key
-if game:HttpGet(check) == "Whitelisted" then
-loadstring(game:HttpGet("https://atriumhub.cloud/script.lua"))()
-else
-game.Players.LocalPlayer:Kick("Invalid Key! Please Rejoin And Try Again.")
-end
