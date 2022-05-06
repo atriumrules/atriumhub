df995964341346dd7e6fb0af847489dc5512e68f
atriumrules <102473631+atriumrules@users.noreply.github.com>
2022-05-06 23:02:24 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,12 @@
-loadstring(game:HttpGet('https://http://atrium.web1337.net/script'))()
+local key = _G.Key
+
+if not key then
+game.Players.LocalPlayer:Kick("Nice try bozo")
+end
+
+local check = "https://http://atrium.web1337.net/check.php?key=" .. key
+if game:HttpGet(check) == "Whitelisted" then
+loadstring(game:HttpGet("https://http://atrium.web1337.net/script.lua"))()
+else
+game.Players.LocalPlayer:Kick("Sorry! This key is incorrect, check the discord for the new key :D")
+end
