002f29968e0ddae9de0819613c6a1d0e9b4aea79
atriumrules <102473631+atriumrules@users.noreply.github.com>
2022-05-07 17:56:27 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,20 +1,4 @@
-local key = _G.key
-
-local url1 = "https://atrium.web/scripts"
-local num = 12
-
-if syn and syn.request then
-
-	local check = "https://http://atrium.web1337.net/check.php?key=" .. key
-
-	if game:HttpGet(check) == "Whitelisted" then
-
-		loadstring(game:HttpGet("https://http://atrium.web1337.net/script.lua"))()
-
-	else
-
-		game.Players.LocalPlayer:Kick("This key is not valid, sorry!")
-
-	end
-
-end
+if _G.Key == "pro"
+	loadstring(game:HttpGet("http://atrium.web1337.net/script.lua"))()
+else:
+	game.Players.LocalPlayer:Kick("Wrong Key")
