2315117db39699be037afb31bb0a2de43c009839
atriumrules <102473631+atriumrules@users.noreply.github.com>
2022-06-25 16:44:45 +0100

    Update loader.lua

--- docs/scripts/loader.lua
+++ docs/scripts/loader.lua
@@ -4,7 +4,7 @@ key = _G.Key
 if key == "test" then
 	Notification.new("success", "Working!", "Loading script...")
 loadstring(game:HttpGet('https://www.atriumhub.cloud/scripts/atrium.lua'))()
-wait(2)
+wait(10)
 Notification.new("info", "Information", "Script Loaded!")
 else
 	Notification.new("error", "Error Code: 404", "You've entered an incorrect key!")
