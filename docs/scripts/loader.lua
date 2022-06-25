4423e459c6941dd8485f32bc96acbeaf91a2dc2d
atriumrules <102473631+atriumrules@users.noreply.github.com>
2022-06-25 18:37:33 +0100

    Update loader.lua

--- docs/scripts/loader.lua
+++ docs/scripts/loader.lua
@@ -1,4 +1,5 @@
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+local player = game:GetService("Players").LocalPlayer
 key = _G.Key 
 
 if key == "test" then
@@ -8,4 +9,12 @@ wait(10)
 Notification.new("info", "Information", "Script Loaded!")
 else
 	Notification.new("error", "Error Code: 404", "You've entered an incorrect key!")
+end
+
+local PremiumUsers = {
+3589438125 -- Owner of Script
+}
+
+if table.find(PremiumUsers, player.UserId) then
+    Notification.new("info", "Premium Commands", "Commands Loaded")
 end
