a31713ff17507c6bddc9a3de178f2347feb766a7
atriumrules <102473631+atriumrules@users.noreply.github.com>
2022-06-25 16:37:23 +0100

    Update and rename atrium.lua to loader.lua

--- docs/scripts/atrium.lua
+++
@@ -1,7 +0,0 @@
-key = _G.Key 
-
-if key == "test" then
-	print("working")
-else
-game.Players.LocalPlayer:Kick("Wrong Key!")
-end
---
+++ docs/scripts/loader.lua
@@ -0,0 +1,12 @@
+_G.Key = "test"
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+key = _G.Key 
+
+if key == "test" then
+	Notification.new("success", "Working!", "Loading script...")
+loadstring(game:HttpGet('https://www.atriumhub.cloud/scripts/atrium.lua'))()
+wait(2)
+Notification.new("info", "Information", "Script Loaded!")
+else
+	Notification.new("error", "Error Code: 404", "You've entered an incorrect key!")
+end
